a874c8c2ef51afa3faee037fea0d839fa5150ab1
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-09-22 21:01:52 +0100

    bounding box

---
+++ BattleTemplate/Assets/Player/Hitsuji15/Asougi/Scripts/AnimeName.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimeName : MonoBehaviour
+{
+    Animator m_Animator;
+    AnimatorClipInfo[] m_AnimatorClipInfo;
+
+    public Text TextFrame;
+
+    float time;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time <= 20)
+        {
+            m_AnimatorClipInfo = m_Animator.GetCurrentAnimatorClipInfo(0);
+            TextFrame.text = string.Format(m_AnimatorClipInfo[0].clip.name);
+        } else 
+        {
+            m_AnimatorClipInfo = m_Animator.GetCurrentAnimatorClipInfo(1);
+            TextFrame.text = string.Format(m_AnimatorClipInfo[0].clip.name);            
+        }
+    }
+}
---
+++ BattleTemplate/Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerInput m_input;
+    Coroutine m_movementCoroutine;
+    [SerializeField] float m_movementSpeed;
+    Rigidbody m_rigidBody;
+    Vector3 m_movementDirection;
+    
+    void Start()
+    {
+        m_input = GetComponent<PlayerInput>();
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_input.currentActionMap.FindAction("Movement").performed += MoveStart;
+        m_input.currentActionMap.FindAction("Movement").canceled += MoveEnd;
+    }
+
+    void MoveStart(InputAction.CallbackContext context)
+    {
+        Debug.Log("outpted");
+        Vector2 m_movement = context.ReadValue<Vector2>();
+        m_movementDirection = new Vector3(m_movement.x, 0, m_movement.y).normalized;
+        m_movementCoroutine = StartCoroutine(Move());
+    }
+
+    void MoveEnd(InputAction.CallbackContext context)
+    {
+        m_movementDirection = Vector3.zero;
+        StopCoroutine(m_movementCoroutine);
+    }
+
+    IEnumerator Move()
+    {
+        while (m_movementDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(m_movementDirection), 1f);
+            transform.position += m_movementDirection * m_movementSpeed * Time.fixedDeltaTime;
+            //m_rigidBody.velocity = new Vector3(m_movementDirection.x * m_movementSpeed * Time.fixedDeltaTime, m_rigidBody.velocity.y, m_movementDirection.z * m_movementSpeed * Time.fixedDeltaTime);
+           
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
