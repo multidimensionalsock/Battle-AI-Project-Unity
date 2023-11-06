05a892d268c1c6b9c257e87b88f9fc1d202319d8
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2023-11-06 17:58:42 +0000

    idle to walk issue

--- BattleTemplate/Assets/AI/Animation/BossAIAnimation.cs
+++ BattleTemplate/Assets/AI/Animation/BossAIAnimation.cs
@@ -23,18 +23,17 @@ public class BossAIAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (lastPos != transform.position)
+        Vector3 distance = transform.position - lastPos;
+        if (Mathf.Abs(distance.x) > 0.01f || Mathf.Abs( distance.z) > 0.01f)//currenrly animatin now working even though it is running the insides
         {
-            Debug.Log("move");
-            lastPos = transform.position;
             m_animator.SetBool("Moving", true);
         }
         else
         {
-            Debug.Log("stop");
             m_animator.SetBool("Moving", false);
         }
         m_animator.SetFloat("YMovement", m_rigidBody.velocity.y);
+        lastPos = transform.position;
     }
 
     void AttackAnimation(Attack attack)
