4c6b80511a575e67958074089aa3452790324b7a
Robyn <92083124+multidimensionalsock@users.noreply.github.com>
2024-01-03 15:45:26 +0000

    bolt

--- BattleTemplate/Assets/AI/MainEnemy/Attacks/Multiple Area Attack/MultipleAreaAttack.cs
+++ BattleTemplate/Assets/AI/MainEnemy/Attacks/Multiple Area Attack/MultipleAreaAttack.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MultipleAreaAttack : MonoBehaviour
 {
+    [SerializeField] int numBolts;
+    [SerializeField] GameObject objectToSpawn;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numBolts; i++)
+        {
+            Vector2 pointInCircle = Random.insideUnitCircle * 10f;
+            GameObject bolt = Instantiate(objectToSpawn);
+            bolt.transform.parent = this.transform; 
+            bolt.transform.position = pointInCircle;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (transform.childCount == 0)
+            Destroy(this);
     }
+
 }
