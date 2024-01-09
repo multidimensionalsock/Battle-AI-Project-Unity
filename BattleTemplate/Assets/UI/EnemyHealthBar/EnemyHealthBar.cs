707e85dc20513be6bc34bfd2918130dd55116ef6
voidrobyn <92083124+AleneVedMidnat@users.noreply.github.com>
2024-01-09 14:07:23 +0000

    .

--- BattleTemplate/Assets/UI/EnemyHealthBar/EnemyHealthBar.cs
+++ BattleTemplate/Assets/UI/EnemyHealthBar/EnemyHealthBar.cs
@@ -8,18 +8,29 @@ public class EnemyHealthBar : MonoBehaviour
 
 	BattleScript m_battleScript;
 	Slider m_slider;
+	Camera m_camera;
+	RectTransform m_rectTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         m_battleScript = transform.GetComponentInParent<BattleScript>();
 		m_slider = GetComponent<Slider>();
+		m_rectTransform= GetComponent<RectTransform>();
 		m_slider.maxValue= m_battleScript.GetHp();
 		m_slider.value = m_slider.maxValue;
 		m_battleScript.HPreduce += ReduceHP;
+		m_camera = Camera.current;
 
     }
 
-    void ReduceHP(float hpLost)
+	private void Update()
+	{
+		Vector3 lookRot = m_camera.transform.position - transform.position;
+		transform.rotation = Quaternion.LookRotation(lookRot);
+	}
+
+	void ReduceHP(float hpLost)
 	{
 		m_slider.value = hpLost;
 		if (m_slider.value <= 0 ) 
