97ccdf462eadcd14f58578bdbd900194c1aed063
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2024-02-13 20:54:16 +0000

    .

--- BattleTemplate/Assets/Player/SpecialAttack/IceAttackHandler.cs
+++ BattleTemplate/Assets/Player/SpecialAttack/IceAttackHandler.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class IceAttackHandler : MonoBehaviour
 {
+    [SerializeField] GameObject iceAttack;
+    [SerializeField] float damage;
+    bool colliding = false;
+    float opacity = 0;
+    [SerializeField] float timeToSpawnNext;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnNext());
     }
 
-    // Update is called once per frame
-    void Update()
+    //function to fade in 
+    //function to fade out
+
+    //ienum to spawn next one after x seconds
+    IEnumerator SpawnNext()
     {
-        
+        yield return new WaitForSeconds(timeToSpawnNext);
+        if (colliding) { yield break; } 
+        //make a new object set spawn to across from 
+        //set rotation
+        //set new trandofmr it it and aaaaaaaa work out where would be across by x
     }
+
+    //on collisoion, if collide then dont make new object and 
+    private void OnTriggerEnter(Collider other)
+    {
+        //might cause issue when it colliders with the floor
+        if (other.gameObject.tag != "Enemy" && other.gameObject.tag != "Floor") { colliding = true; }
+
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerBattleScript>().Attack(damage * opacity);
+        }
+
+    }
+
+    //damage is affected by object opacity 
+
 }
